ee04725cc85f3e7dc451dcbb4b8fb834f0d10680
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-07-31 16:15:32 -0700

    day3afternoon 2

--- practice.js
+++ practice.js
@@ -17,7 +17,10 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  
+  return arr[0]
+}
 
 
 
@@ -32,7 +35,10 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[2]
+
+}
 
 
 
@@ -47,7 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for (let i = 0; i < family.length; i++)
+    alert(family[i])
+  
+};
 
 
 
@@ -62,7 +72,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){
+  for (let i = letters.length -1; i >= 0; i-- )
+    alert(letters[i])
+}
 
 
 
@@ -77,7 +90,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  
+  var container = []
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0){
+    container.push(nums[i]);
+
+  }
+}
+return container
+}
 
 
 
